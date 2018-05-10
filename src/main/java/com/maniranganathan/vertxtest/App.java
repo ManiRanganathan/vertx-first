31b5ae43401ff49172ae9ebc5a1413f6256f22ed
Manivanna Ranganathan <mranganathan17@gmail.com>
2018-05-09 19:59:44 -0700

    Updated the vertx related code

--- src/main/java/com/maniranganathan/vertxtest/App.java
+++ src/main/java/com/maniranganathan/vertxtest/App.java
@@ -1,13 +1,27 @@
 package com.maniranganathan.vertxtest;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+        // Create the Vert.x instance
+        Vertx vertx = Vertx.vertx();
+
+        // Create a router
+        Router router = Router.router(vertx);
+        router.get("/").handler(rc -> rc.response().end("Hello"));
+        router.get("/:name").handler(rc -> rc.response().end("Hello " + rc.pathParam("name")));
+
+        // Start the HTTP server.
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(8080);
     }
 }
